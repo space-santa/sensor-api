714568551ba0ba9b688dcbdc4bbc5d866e2e1020
claus <claus.z@protonmail.com>
2019-03-16 16:18:37 +1100

    Only allow admin to register devices

--- SensorApi/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SensorApi/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,18 +71,7 @@ namespace SensorApi.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { userId = user.Id, code = code },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    await _userManager.AddToRoleAsync(user, Constants.DeviceRole);
                     return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
