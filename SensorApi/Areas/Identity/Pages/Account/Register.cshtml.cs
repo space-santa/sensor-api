70795b12f8e75683d8b8dca6ea27e890591b666d
claus <claus.z@protonmail.com>
2019-03-23 14:02:24 +1100

    Remove unused using

--- SensorApi/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SensorApi/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 
 namespace SensorApi.Areas.Identity.Pages.Account
